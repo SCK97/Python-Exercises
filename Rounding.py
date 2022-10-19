4266269d3b33f5c403658f69bf86ab2481729fc7
SCK97 <shaunacurryk@gmail.com~>
2022-10-19 17:08:33 +0100

    Rounding.py

---
+++ Rounding.py
@@ -0,0 +1,13 @@
+number1 = input("Enter whole number:")
+number2 = input("Enter decimal number:")
+
+int_num = int(number1)
+float_num = float(number2)
+round_num = int(round(float_num))
+
+print(int_num,float_num,round_num)
+
+#i think the output will be 
+    #Enter whole number:
+    #Enter decimal number:
+    #whole number, decimal number, decimal number but rounded to nearest whole number
