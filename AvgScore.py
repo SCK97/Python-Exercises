3813e62188899d7dfecfe2ce06c5fe0124959c27
SCK97 <shaunacurryk@gmail.com~>
2022-10-19 16:46:27 +0100

    AvgScore.py

---
+++ AvgScore.py
@@ -0,0 +1,8 @@
+English = input("Enter your Biology score: ")
+Maths = input("Enter your Maths score: ")
+Science = input("Enter your Science score: ")
+
+total = int(English) + int(Maths) + int(Science)
+avg = total/3
+
+print("Your average score is:", avg)
