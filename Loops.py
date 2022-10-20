9c5a2e01665c965ddfdec140f2de53ce48cd1b73
SCK97 <shaunacurryk@gmail.com~>
2022-10-20 14:51:07 +0100

    Loops.py

---
+++ Loops.py
@@ -0,0 +1,20 @@
+#for i in range(101):
+#    print(i)
+
+#will print out each number in this range, from 0 to 100 as python starts at 0
+
+
+
+#for i in range(5,10):
+#    print(i)
+
+#will print each number between 5 and 10, excluding 10
+#so 5, 6, 7, 8, 9
+
+
+
+for i in range(10,21,2):
+    print(i)
+
+#will print numbers from 10 to 21 in increments of 2, excluding 21
+#s0 10, 12, 14, 16, 18, 20
