66c01324db6402c9c1ae4463b7a78d9811819a9f
SCK97 <shaunacurryk@gmail.com~>
2022-10-20 13:46:39 +0100

    w3r-q1.py

---
+++ w3r-q1.py
@@ -0,0 +1,6 @@
+#https://www.w3resource.com/python-exercises/string/
+#1. Write a Python program to calculate the length of a string.
+
+s = input("Enter some text: ")
+
+print("You typed ", len(s), " characters")
