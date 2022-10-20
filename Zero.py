831246a1e9455e227bdab3e75152c121fd1a031e
SCK97 <shaunacurryk@gmail.com~>
2022-10-20 14:54:48 +0100

    Zero.py

---
+++ Zero.py
@@ -0,0 +1,14 @@
+#count = 0
+
+#while count < 3:
+#    print(count)
+
+#will continue printing 0 forever as it will always be less than 3
+
+
+
+count = 0
+
+while count <= 3:
+    print(count)
+    count+=1
